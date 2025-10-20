0a70f0525ef68840e3430a52175db53a215dfd3d
Luca Becker <luca.becker@me.com>
2025-10-20 17:29:41 +0200

    fix(deps): update dependency @astrojs/check to v0.9.5

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: [['html', { open: 'never' }], ['list'], ...(process.env.CI ? [['github' as const]] : [])],
+    reporter: [['html', { open: 'never' }], ['list'], ...(process.env.CI ? ([['github']] as const) : [])],
     use: {
         baseURL,
         trace: 'on-first-retry',
